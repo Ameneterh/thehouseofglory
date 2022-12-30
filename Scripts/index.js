957b2aa14884ab47c05d20d5a4abca60ee64f10a
ameneterh <ameneterh@gmail.com>
2022-12-30 22:48:10 +0100

    More styling - header/footer, adding contact page

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,8 @@
 var year = new Date().getFullYear();
 document.getElementById("copyrightYear").innerHTML = year;
 
+var contact = document.getElementById("contact");
+var drop = document.getElementById("dropdown");
+
+function onHover() {}
 // nothing yet
