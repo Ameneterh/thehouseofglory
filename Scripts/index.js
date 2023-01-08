62c44fcb3406a9d3420094beed19296eedc1c89f
ameneterh <ameneterh@gmail.com>
2023-01-08 23:21:23 +0100

    adding content to index page

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,6 +3,3 @@ document.getElementById("copyrightYear").innerHTML = year;
 
 var contact = document.getElementById("contact");
 var drop = document.getElementById("dropdown");
-
-function onHover() {}
-// nothing yet
