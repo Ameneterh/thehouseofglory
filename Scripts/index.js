e8b44b247647759c54f9bb72435ea23500ea7152
ameneterh <ameneterh@gmail.com>
2023-01-14 19:38:23 +0100

    Changing to mobile first dev

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,3 +3,24 @@ document.getElementById("copyrightYear").innerHTML = year;
 
 var contact = document.getElementById("contact");
 var drop = document.getElementById("dropdown");
+
+// function for dropdown menu
+
+function getDropMenu() {
+  var menu = document.getElementById("drop-down-menu");
+
+  if (menu.style.display == "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
+
+// set dropdown to none on click outside of the menu
+// var drop = document.getElementById("drop-down-menu");
+
+// window.onclick = function (event) {
+//   if (event.target == drop) {
+//     drop.style.display = "none";
+//   }
+// };
